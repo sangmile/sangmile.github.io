e9f19e44bbd38e1167c59fab419f151b46df183c
it1258 <sangmile@gsitm.com>
2020-10-14 01:40:01 +0900

    commit at home

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Jekyll GitHub Blog",
+        "excerpt":"Jekyll Blog 설정 Git Repository 생성 GitHub에서 새 repository 만들기 repository name: &lt;yourname&gt;.github.io 예)sangmile.github.io git init 명령어 git init sangmile.github.io cd sangmile.github.io repo 추가 명령어 git remote add origin https://github.com/sangmile/sangmile.github.io.git master branch 생성 git checkout -b master Jekyll Theme 설정 _posts 폴더 생성 _posts 폴더 아래에 새 글을 작성할 것임....","categories": ["jekyll","configuration"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/configuration/first-post/",
+        "teaser": null
+      }]
